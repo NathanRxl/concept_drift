b0a5eeeea88cbf9cc570de73b0e5860abffbd0ee
NathanRxl <nathan.rouxel@gmail.com>
2017-01-28 20:06:16 +0100

    main: Add AdapativeSVC to the comparison

--- main.py
+++ main.py
@@ -3,17 +3,21 @@ from StreamGenerator import StreamGenerator
 from DataLoader import SEALoader
 from AlgorithmsComparator import AlgorithmsComparator
 from ensemble_methods import SEA
+from AdaptiveSVC import AdaptiveSVC
 
 # generate data
-loader = SEALoader('../data/sea.data')
+loader = SEALoader('data/sea.data')
 generator = StreamGenerator(loader)
 
 # models
 SEA1 = SEA(10)
 SEA2 = SEA(10, base_estimator=SVC())
+ASVC = AdaptiveSVC(C=100, memory_limit=15000)
+
 algorithms = [
     ("SEA (Decision Tree)", SEA1),
-    ("SEA (SVC)", SEA2)
+    ("SEA (SVC)", SEA2),
+    ("Adaptive SVC", ASVC),
 ]
 
 # comparison of algorithms
